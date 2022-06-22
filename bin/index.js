cce3c4eb17adb3f8ac03d045a26a40429394266a
konglong127 <1628253184@qq.com>
2022-06-22 19:37:21 +0800

    finish

--- bin/index.js
+++ bin/index.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
-// npm link 连接工具
+// npm link 连接工具 npm unlink取消连接
 const program=require('commander');
 
 program.version(require('../package.json').version);
 
 // 命令行定制
+
 program
   .command('create <name>')
-  .description('create server template')
+  .description('create server template.')
   .action(require('../lib/init'))
 
 program
   .command('build project')
-  .description('build project')
+  .description(`input 'template build project' under porject folder.`)
   .action(require('../lib/build'))
 
 program.parse(process.argv);
---
+++ readme.md
@@ -0,0 +1,17 @@
+# lkst
+
+A Koa server template cli,it's a very easy server template and it can build http、http2、https and support typescript.
+
+# Install
+
+```
+npm install lkst -g
+```
+
+# Usage
+
+```
+lkst create <project>
+lkst build project
+```
+
